9e5c1ae023d86d43ba41f24f5e661a0a817c5b4b
Kush Singh <kushsingh@Kushs-MacBook-Air.local>
2023-03-28 17:00:11 +0530

    Initial commit

---
+++ index.js
@@ -0,0 +1,9 @@
+var x = document.querySelectorAll("bottombar")whatsapp;
+
+document.querySelectorAll("bottombar")whatsapp.addEventListener("click", function(){
+  alert("Sorry, not avialable");
+});
+
+document.addEventListener("keypress", function () {
+  alert("Key was pressed!");
+})
